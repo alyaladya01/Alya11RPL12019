3c2687725006316b42468e868790327e7e4d80e0
alyaladya01 <nisrinaalya0112@gmail.com>
2020-09-08 07:04:06 +0700

    Initial commit

---
+++ app/src/main/java/com/example/nisrinalyaladya01/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.nisrinalyaladya01;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    SharedPreferences pref;
+    SharedPreferences.Editor editor;
+
+    EditText txtusername;
+    EditText txtpassword;
+    Button btnlogin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        pref = getSharedPreferences("login", MODE_PRIVATE);
+        txtusername = (EditText)findViewById(R.id.txtusername);
+        txtpassword = (EditText)findViewById(R.id.txtpassword);
+        btnlogin = (Button) findViewById(R.id.btnlogin);
+        btnlogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (txtusername.getText().toString().equalsIgnoreCase("alya")
+                        && txtpassword.getText().toString().equalsIgnoreCase("0112")) {
+                    //saving ke SP
+                    editor = pref.edit();
+                    editor.putString("username", txtusername.getText().toString());
+                    editor.putString("status", "login");
+                    editor.apply();
+                    //menuju ke main menu
+                    startActivity(new Intent(getApplicationContext(), MainMenu.class));
+                    finish();
+                }
+            }
+        });
+
+    }
+}
